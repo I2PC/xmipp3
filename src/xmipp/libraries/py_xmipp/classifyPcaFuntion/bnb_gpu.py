572164c952339027c6ac71dcd555c8271ce36f48
erneyramirez <erney.ramirez@gmail.com>
2025-07-15 11:14:06 +0200

    18 iter, thr 2.0, buttercombined(20_order 2-include filter), multiresol, create classes with 90000 particles

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -1960,7 +1960,7 @@ class BnBgpu:
                 # expBatchSize = 10000
                 expBatchSize2 = 20000
                 # numFirstBatch = 2
-                numFirstBatch = 4
+                numFirstBatch = 6
             elif dim <= 256:
                 expBatchSize = 4000 
                 expBatchSize2 = 5000
