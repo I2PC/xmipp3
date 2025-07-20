cd146b7e035c1c2a17d3ff97c8e8f038463feacd
erneyramirez <erney.ramirez@gmail.com>
2025-07-20 13:12:09 +0200

    18 iter, thr 2.0, butterCombined-cos-nq_FFT, multiresol, create classes with 75000 particles

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -1898,11 +1898,12 @@ class BnBgpu:
         lp_filter = 1.0 / (1.0 + (r_norm_exp / (frc_cutoffs + eps)) ** (2 * order))  # [B, H, W]
     
         # --- Filtro de realce tipo coseno ---
-        enhance_filter = torch.where(
-            r_norm_exp <= frc_cutoffs,
-            0.5 * (1 - torch.cos(torch.pi * r_norm_exp / (frc_cutoffs + eps))),
-            torch.ones_like(r_norm_exp)
-        )
+        # enhance_filter = torch.where(
+        #     r_norm_exp <= frc_cutoffs,
+        #     0.5 * (1 - torch.cos(torch.pi * r_norm_exp / (frc_cutoffs + eps))),
+        #     torch.ones_like(r_norm_exp)
+        # )
+        enhance_filter = 0.5 * (1 - torch.cos(torch.pi * r_norm_exp))
         bp_filter = enhance_filter  # [B, H, W]
     
         # === FFT del original ===
