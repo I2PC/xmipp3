817b9c01aab925cf5799746d3cef8af0ae91a855
erneyramirez <erney.ramirez@gmail.com>
2025-07-29 14:01:08 +0200

    18 iter, thr 2.0, butterCombine(nq-20), mask restrictiva, multiresol, create classes with 75000 particles

--- src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
+++ src/xmipp/libraries/py_xmipp/classifyPcaFuntion/bnb_gpu.py
@@ -529,8 +529,8 @@ class BnBgpu:
         # if 3 < iter < 7 and iter % 2 == 0:
         if 3 < iter < 7 and iter % 2 == 0:
             # clk = clk * self.approximate_otsu_threshold(clk, percentile=10)
-            clk = clk * self.contrast_dominant_mask(clk, window=3, contrast_percentile=90,
-                                intensity_percentile=60, contrast_weight=1.5, intensity_weight=1.0, smooth_sigma=1.0)
+            clk = clk * self.contrast_dominant_mask(clk, window=3, contrast_percentile=80,
+                                intensity_percentile=50, contrast_weight=1.5, intensity_weight=1.0, smooth_sigma=1.0)
 
         
         # if iter < 17:
