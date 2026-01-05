a06faf0f4f6c9bbbd992c74ce477213f48d8477b
Ratolon <miceta@cnb.csic.es>
2026-01-05 14:01:47 +0100

    Model management bases

---
+++ src/xmipp/libraries/py_xmipp/deepPickingConsensusTomo/dpc3d_model_manager.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""""
+**************************************************************************
+*
+* Authors:  Mikel Iceta Tena (miceta@cnb.csic.es)
+* 
+*
+* Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+* 02111-1307  USA
+*
+*  All comments concerning this program package may be sent to the
+*  e-mail address 'scipion@cnb.csic.es'
+*
+* Initial version: jan 2025
+**************************************************************************
+"""
+from dpc3d_data_manager import * # For the data loader
+from dpc3d_model_definitions import * # For the architecture skeletons
+from dpc3d_parameter_manager import * # For estimating NN parameters
+
+import tensorflow as tf
+from tensorflow.keras import mixed_precision
+
+# Memory management tricks
+mixed_precision.set_global_policy('mixed_float16')
+
+class ModelManDPC3D:
+    pass
+
+
