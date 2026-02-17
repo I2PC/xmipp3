65477353a544a4d6facc758d0628395fd7749f5f
erneyramirez <erney.ramirez@gmail.com>
2026-02-17 10:36:59 +0100

    only circular mask

--- src/xmipp/applications/scripts/alignPCA_train/batch_train_pca.py
+++ src/xmipp/applications/scripts/alignPCA_train/batch_train_pca.py
@@ -140,7 +140,7 @@ if __name__=="__main__":
         
         expImages = mexp.data[initBatch:endBatch].astype(np.float32)#.copy()
         Texp = torch.from_numpy(expImages).float().to(cuda)
-        # radius = 60
+        radius = 60
         Texp = Texp * bnb.create_mask(Texp, radius)
         # Texp = bnb.robust_normalize_and_mask(Texp)
         del(expImages)
